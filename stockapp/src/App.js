10c3c736c5b4fbb4b663d19ec46847ac7b0a2ea3
Lee <suemlee@amazon.com>
2020-10-05 19:46:34 -0700

    Enable react-bootstrap

--- stockapp/src/App.js
+++ stockapp/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { ButtonToolbar, Button } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
@@ -16,16 +16,27 @@ class App extends Component {
     const { username } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
           <div>
-              {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
+              {username ? <h1>Stock web - {`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
           </div>
+
+          <ButtonToolbar>
+          {/* Standard button */}
+          <Button>Default</Button>
+          {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+          <Button bsStyle="primary">Primary</Button>
+          {/* Indicates a successful or positive action */}
+          <Button bsStyle="success">Success</Button>
+          {/* Contextual button for informational alert messages */}
+          <Button bsStyle="info">Info</Button>
+          {/* Indicates caution should be taken with this action */}
+          <Button bsStyle="warning">Warning</Button>
+          {/* Indicates a dangerous or potentially negative action */}
+          <Button bsStyle="danger">Danger</Button>
+          {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
+          <Button bsStyle="link">Link</Button>
+        </ButtonToolbar>
+
       </div>
     );
   }
