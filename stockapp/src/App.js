e4f808f06143867647b356a2a1b48dc1d44f74f7
Lee <suemlee@amazon.com>
2020-10-11 15:29:59 -0700

    Enable react bootstrap

--- stockapp/src/App.js
+++ stockapp/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { ButtonToolbar, Button } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+//import Form from 'react-bootstrap/Form';
+//import FormControl from 'react-bootstrap/FormControl';
+//import Button from 'react-bootstrap/Button'
 import './App.css';
 
 class App extends Component {
@@ -13,30 +17,16 @@ class App extends Component {
     }
 
   render() {
-    const { username } = this.state;
+    //const { username } = this.state;
     return (
       <div className="App">
-          <div>
-              {username ? <h1>Stock web - {`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-          </div>
 
-          <ButtonToolbar>
-          {/* Standard button */}
-          <Button>Default</Button>
-          {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
-          <Button bsStyle="primary">Primary</Button>
-          {/* Indicates a successful or positive action */}
-          <Button bsStyle="success">Success</Button>
-          {/* Contextual button for informational alert messages */}
-          <Button bsStyle="info">Info</Button>
-          {/* Indicates caution should be taken with this action */}
-          <Button bsStyle="warning">Warning</Button>
-          {/* Indicates a dangerous or potentially negative action */}
-          <Button bsStyle="danger">Danger</Button>
-          {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
-          <Button bsStyle="link">Link</Button>
-        </ButtonToolbar>
-
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">STOCK WEB</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Stock Info</Nav.Link>
+          </Nav>
+        </Navbar>
       </div>
     );
   }
